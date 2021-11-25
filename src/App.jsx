2c9527e52c50d57fa90d2c37ffcf25ac1b242112
Caroline COPY-DENHEZ <caroline.copy19@gmail.com>
2021-11-25 17:27:22 +0100

    Make modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import Ennemy from './components/game/Ennemy';
+import Ennemy from './components/Game/Ennemy';
+import Home from './components/Home';
+import './App.css';
 
 function App() {
   return (
     <>
+      <Home />
       <h1>Componante APP</h1>
       <Ennemy />
     </>
