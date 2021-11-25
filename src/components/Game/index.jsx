8e06e4b797e63fb7ea67e819aee518bf41b6f894
NacymeG <nacyme.gallel@gmail.com>
2021-11-26 00:40:03 +0100

    Rendering AttackButton

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom';
 import Player from './Player';
 import Ennemy from './Ennemy';
 import Button from './Button';
+import AttackButton from './AttackButton';
 import './style.css';
 
 export default function Game() {
   const [playerLvl, setPlayerLvl] = useState(0);
-
   return (
     <>
       <h1 className="readyFight">READY? FIGHT!</h1>
       <div className="fightContain">
         <Player playerLvl={playerLvl} setPlayerLvl={setPlayerLvl} />
+        <AttackButton />
+
         <Ennemy />
       </div>
       <Link to="/ChooseChar">
