6106b6089b8bd43346a778228fc5db86c8bd2149
Pierre Goemans <p.goemans@hotmail.fr>
2021-11-25 12:37:26 +0100

    Structure initiale

---
+++ src/components/game/AttackButton/index.jsx
@@ -0,0 +1,3 @@
+export default function AttackButton() {
+  return <div>conponent AttackButton</div>;
+}
---
+++ src/components/game/Ennemy/index.jsx
@@ -0,0 +1,3 @@
+export default function Ennemy() {
+  return <div>conponent Ennemy</div>;
+}
