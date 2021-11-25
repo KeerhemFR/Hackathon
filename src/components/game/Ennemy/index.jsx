9f6a154d8b219ce3c8143711283fca403cb39ee8
Guillaume Keerhem <keerhem.guillaume@hotmail.fr>
2021-11-25 15:34:11 +0100

    Made tha api call of the enemy

--- src/components/game/Ennemy/index.jsx
+++ src/components/game/Ennemy/index.jsx
@@ -1,3 +1,28 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function Ennemy() {
-  return <div>conponent Ennemy</div>;
+  const [ennemyInfos, setEnnemyInfos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://cors-anywhere.herokuapp.com/api.deezer.com/artist/27')
+      .then(({ data }) => {
+        setEnnemyInfos(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  console.log(ennemyInfos);
+
+  const ennemyHp = Math.floor(ennemyInfos.nb_fan / 10000);
+
+  return (
+    <>
+      <img src={ennemyInfos.picture_big} alt="" />
+      <div>{ennemyInfos.name}</div>
+      <p>{ennemyHp}</p>
+    </>
+  );
 }
