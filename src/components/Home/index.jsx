0a379436dfd91f596ff43898d88477a8ba5a4c7c
Caroline COPY-DENHEZ <caroline.copy19@gmail.com>
2021-11-25 21:05:43 +0100

    Update style of HomePage with new logotype

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,4 +1,3 @@
-import logoBackground from '../Assets/logoBackground.png';
 import omgLogo from '../Assets/omgLogo.png';
 import './style.css';
 
@@ -6,7 +5,6 @@ export default function Home() {
   return (
     <div className="logo">
       <img className="omgLogo" src={omgLogo} alt="omg" />
-      <img className="logoType" src={logoBackground} alt="logotype" />
     </div>
   );
 }
