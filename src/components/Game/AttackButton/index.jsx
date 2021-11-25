529c01b3e815c450110b4625cd00f699d2e9a9aa
NacymeG <nacyme.gallel@gmail.com>
2021-11-26 00:37:13 +0100

    Init context

--- src/components/Game/AttackButton/index.jsx
+++ src/components/Game/AttackButton/index.jsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import GameContext from 'context/GameContext';
+
 export default function AttackButton() {
+  const { progress, setProgress, clickPower } = useContext(GameContext);
   return (
     <div>
       <button
         className="attackBtn"
         type="submit"
-        // onClick={() => setProgress(progress - clickPower)}
+        onClick={() => setProgress(progress - clickPower)}
       >
         Attack
       </button>
