c052124e8952bb7a6387c5810947ea65e356c7f1
Pierre Goemans <p.goemans@hotmail.fr>
2021-11-25 17:24:16 +0100

    Fix merge Quentin

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Game from './components/Game';
+import Button from './components/Game/Button';
+
 import './reset.css';
 import './App.css';
-
-
 
 function App() {
   return (
     <>
       <h1>Componante APP</h1>
       <Game />
+      <Button />
     </>
   );
 }
--- src/components/Game/Player/index.jsx
+++ src/components/Game/Player/index.jsx
@@ -34,4 +34,3 @@ export default function Player() {
 //   playerLvl: [],
 //   setPlayerLvl: () => {},
 // };
-
