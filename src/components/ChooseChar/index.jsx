2a5088f393c8b6a49e8fbb75f5519acf456666cc
Quentin Macquart <macquart.quentin@gmail.com>
2021-11-26 11:03:53 +0100

    FINISH CSS DESKTOP

--- src/components/ChooseChar/index.jsx
+++ src/components/ChooseChar/index.jsx
@@ -1,18 +1,23 @@
 import { Link } from 'react-router-dom';
 import Button from '../Game/Button';
 import SearchBar from './SearchBar';
+import './style.css';
 
 export default function Choosechar() {
   return (
     <>
-      <h1>Choose your singer</h1> <br />
-      <SearchBar />
-      <Link to="/Game">
-        <Button content="Play" />
-      </Link>
-      <Link to="/">
-        <Button content="Return" />
-      </Link>
+      <h1 className="chooseSinger">Choose your singer</h1> <br />
+      <div className="chooseCharWrapper">
+        <SearchBar />
+        <div className="searchButtonWrapper">
+          <Link to="/Game">
+            <Button className="searchBarButton" content="Play" />
+          </Link>
+          <Link to="/">
+            <Button className="searchBarButton" content="Return" />
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
